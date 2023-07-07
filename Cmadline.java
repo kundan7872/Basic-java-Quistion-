a9ab08857b6f47b24f2a23f9dcb6802120301fa8
Kundan Kumar Singh <128737646+kundan7872@users.noreply.github.com>
2023-07-08 00:03:09 +0530

    Print some String
    Give output using command line in java

---
+++ Cmadline.java
@@ -0,0 +1,9 @@
+class Cmadline{
+public static void main(String []args){
+
+System.out.println("First int is "+args[0]); // java class name hello 
+System.out.println("Second int is "+args[1]);// kundan
+System.out.println("Have a good day");
+}
+}
+// output hello kundan Have a good day
